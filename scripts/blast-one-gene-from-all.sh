52bc8b5820831d912d0d4cd11886bf7e778ca4d0
Zebulun Arendsee <arendsee@iastate.edu>
2017-04-23 17:46:03 -0500

    Add columns to 1-protein-vs-all output

--- scripts/blast-one-gene-from-all.sh
+++ scripts/blast-one-gene-from-all.sh
@@ -3,21 +3,24 @@ module load ncbi-blast
 
 [[ -d smof ]] || git clone https://github.com/arendsee/smof
 
-protein_name="NADH dehydrogenase subunit 6"
 all_pro='data_mitochondria/mitochondrion*protein.faa'
 
-my_pro=data_mitochondria/nd6-mito.faa
+base=nd6_2
+protein_name="NADH dehydrogenase subunit 6";
+
+blastresult=${base}-blast.tab
+my_pro=data_mitochondria/${base}-mito.faa
 
 cat $all_pro | smof/smof.py grep "$protein_name" > $my_pro
 
-tblastn                  \
-    -task tblastn-fast   \
-    -query $my_pro       \
-    -db blastdb/bioblitz \
-    -evalue 1e-2         \
-    -num_threads 8       \
-    -max_target_seqs 1e8 \
-    -outfmt '6 qseqid sseqid qlen slen qstart qstop sstart sstop bitscore evalue' > nd6-blast.tab
+time tblastn                    \
+    -task tblastn-fast          \
+    -query $my_pro              \
+    -db blastdb/bioblitz        \
+    -evalue 0.001               \
+    -num_threads 8              \
+    -max_target_seqs 1000000000 \
+    -outfmt '7 qseqid sseqid qlen slen qstart qstop sstart sstop bitscore pident positive evalue' > $blastresult
 
 # ref|YP_009231648.1| NADH dehydrogenase subunit 2 (mitochondrion) [Camponotus atrox]
 # ref|YP_009231649.1| cytochrome c oxidase subunit I (mitochondrion) [Camponotus atrox]
