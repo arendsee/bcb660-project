4eb919dab3e9d89a2305f65c7edd9d0b6bf26bd4
Zebulun Arendsee <arendsee@iastate.edu>
2017-05-05 17:23:54 -0500

    Add taxomony analysis to mitochondrial stat script

--- scripts/mitochondrial-stats.sh
+++ scripts/mitochondrial-stats.sh
@@ -1,6 +1,7 @@
 set -u
 
 mitodir=data_mitochondria
+sample=sample-taxonomies.tab
 
 statdir=mito-stats
 
@@ -10,6 +11,8 @@ species_list=$statdir/species-list.txt
 gene_counts=$statdir/gene-counts.tab
 irregular_names=$statdir/irregular-names.tab
 mitorep=$statdir/mitochondria-report.txt
+
+mitoclades=$statdir/mito-clade-counts.tab
 
 mitoprot=$mitodir/*protein*faa
 
@@ -48,3 +51,52 @@ make-report () {
 [ -f $irregular_names ] || list-irregular-species-names > $irregular_names
 [ -f $gene_counts     ] || count-genes                  > $gene_counts
 [ -f $mitorep         ] || make-report                  > $mitorep
+
+
+
+# ==================== T A X O N O M I C   R E P O R T ========================
+
+if [ ! -d litade ]
+then
+    git clone https://github.com/arendsee/litade
+    cd litade
+    ./setup.sh
+    cd ..
+fi
+litade=$PWD/litade/litade.pl
+
+if [ ! -f taxid2sciname.tab ]
+then
+    wget -O taxdmp.zip ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip
+    unzip -p taxdmp.zip names.dmp > names.dmp
+    # Build a map from Taxon Id to Scientific name
+    sed 's/[\t ]*|[\t ]*/\t/g' names.dmp |
+        awk '
+            BEGIN{FS="\t"; OFS="\t"}
+            /scientific name/ {print $1,$2}
+        ' > taxid2sciname.tab
+fi
+
+sample_taxids="inputs/sample-taxids.tab" 
+mito_taxids=mito-taxids.txt
+protein_gpff=$mitodir/*protein*gpff
+
+join -t $'\t' \
+     <(sort $sample_taxids) \
+     <($litade $(cut -f 1 $sample_taxids) | sort) |
+    tee z |
+    awk 'BEGIN{FS="\t"; OFS="\t"} {print $1,$2}' | sort | uniq -c |
+    sed 's/  *//' | sed 's/ /\t/' | 
+    awk 'BEGIN{FS="\t"; OFS="\t"} {print $2,$3,$1}' | sort > a
+
+join -1 1 -2 3 -t $'\t' \
+     <(sed -nr 's/.*db_xref="taxon:([0-9]+).*/\1/p' $protein_gpff | sort -u) \
+     <(sort -t $'\t' -k3 z) |
+    cut -f2 | sort | uniq -c |
+    awk 'BEGIN{OFS="\t"} {print $2,$1}' > b
+
+
+echo -e "taxid\tclade\ttotal\tsequenced" > $statdir/taxid-counts.tab
+join -t $'\t' <(sort a) <(sort b) >> $statdir/taxid-counts.tab
+
+rm a b z
