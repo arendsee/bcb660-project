c712f9eada28a5f8e3edcecf97eea2c7e7249890
Zebulun Arendsee <arendsee@iastate.edu>
2017-05-05 13:55:58 -0500

    Fix mitochondrial stat script

--- scripts/mitochondrial-stats.sh
+++ scripts/mitochondrial-stats.sh
@@ -1,30 +1,50 @@
+set -u
+
 mitodir=data_mitochondria
-mitorep=mitochondria-report.txt
 
-species_list=species-list.txt
+statdir=mito-stats
 
->$mitorep
+[ -d $statdir ] || mkdir $statdir
 
-count_them () {
-    echo -n "Number of represented mitochondrial genomes: "
-    zcat $mitdir/*protein*faa.gz |
-        sed 's/.*\[\(.*\)\]$/\1/' |
-        uniq | sort -u | tee $species_list | wc -l
+species_list=$statdir/species-list.txt
+gene_counts=$statdir/gene-counts.tab
+irregular_names=$statdir/irregular-names.tab
+mitorep=$statdir/mitochondria-report.txt
+
+mitoprot=$mitodir/*protein*faa
+
+list-species () {
+    sed -nr 's/^>.*\[(.*)\]$/\1/p' $mitoprot |
+        uniq | sort -u
 }
 
-count_genes () {
-    echo "Number of genomes with n proteins:"
+count-genes () {
     echo -e "count\tn_proteins"
-    zcat $mitdir/*protein*faa.gz |
-        sed 's/.*\[\(.*\)\]$/\1/' |
-        sort | uniq -c | tr ' ' "\t" | sort -rnk2
+    sed -nr 's/^>.*\[(.*)\]$/\1/p' $mitoprot |
+        sort | uniq -c |
+        awk '{print $1}' |
+        sort -n | uniq -c | sort -rn |
+        sed 's/^ *//; s/  */\t/'
 }
 
-list_irregular_species_names () {
+list-irregular-species-names () {
+    grep -vP '^[A-Z][a-z]+ [a-z]+$' $species_list | sed 's/^/  /'
+}
+
+make-report () {
+    echo -n "Number of represented mitochondrial genomes: "
+    wc -l $species_list | sed 's/ .*//'
+
+    echo
+    echo "First 10 most common mitochondrial gene counts"
+    head -11 $gene_counts
+
+    echo
     echo "Species with irregular names (i.e. not '[A-Z][a-z]+ [a-z]+'):"
-    grep -P '^[A-Z][a-z]+ [a-z]+$' $species_list | sed 's/^/  /'
+    wc -l $irregular_names | sed 's/ .*//'
 }
 
-count_them >> $mitorep
-count_genes >> $mitorep
-list_irregular_species_names >> $mitorep
+[ -f $species_list    ] || list-species                 > $species_list
+[ -f $irregular_names ] || list-irregular-species-names > $irregular_names
+[ -f $gene_counts     ] || count-genes                  > $gene_counts
+[ -f $mitorep         ] || make-report                  > $mitorep
